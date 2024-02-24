0783fb771c9678e6befeea336332ca3349bb3135
whirlsun <whirlsun@qq.com>
2024-02-24 09:50:41 +0800

    merge

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-<<<<<<< HEAD
 # Register your models here.
 # admin.site.register(Question)
 
@@ -28,7 +27,4 @@ class QuestionAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Question, QuestionAdmin)
-=======
-admin.site.register(Question)
->>>>>>> refs/remotes/origin/main
 admin.site.register(Choice)
--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path,include
 from . import views
 
-<<<<<<< HEAD
 app_name = "polls"
 urlpatterns = [
     path('', views.index, name='index'),
@@ -9,12 +8,4 @@ urlpatterns = [
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
     path("__debug__/", include("debug_toolbar.urls")),
-=======
-app_name = 'polls'
-urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
->>>>>>> refs/remotes/origin/main
 ]
