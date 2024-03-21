8e4706d4bd2dfac36ad453770cab644951b94853
whirlsun <whirlsun@qq.com>
2024-03-21 23:25:21 +0800

    modify views.py

--- polls/views.py
+++ polls/views.py
@@ -82,3 +82,7 @@ def seethesea(arg):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/results.html", {"question": question})
+
+def test():
+    pass
+
